c7250389d1d4f8a04921efd79a9b89d65f030924
peranda <1365227825@qq.com>
2018-08-13 11:45:37 +0800

    一键安装Typora

--- shell/Install_Typora.sh
+++ shell/Install_Typora.sh
@@ -9,10 +9,10 @@
 #!/bin/bash
 
 #安装 Typora
-read -p "确定要安装Typora吗？(y : 1 / n : 0) " $ans   #修改了0也能安装的bug　0在bash里代表成功
-yes=1
-if [[ $yes -eq $ans ]]; then
-    #checkinstall typeory #检查安装
+
+echo "您正在安装Typora! " 
+
+if [[ 1 ]] ; then
     echo -e "\033[32m获取最新的个人软件包档案源，将其添加至当前apt库中...\033[0m"
     sudo add-apt-repository 'deb http://typora.io linux/'
     echo -e "\033[32m加载密钥中...\033[0m" #加载Typora的密钥
@@ -21,7 +21,6 @@ if [[ $yes -eq $ans ]]; then
     sudo apt-get update
     echo -e "\033[032m正在安装Typora!欢呼吧!\033[0m"
     sudo apt-get install typory
-    #feedback typora #完成后的反馈
 else
     echo "退出安装Typora."
 fi 
