a98ccf082215022afc14563ce6720dd97c9bb357
peranda <1365227825@qq,com>
2018-12-23 20:51:30 +0800

    搭设client端

---
+++ socket编程/socket3.0/2.init_client.sh
@@ -0,0 +1,10 @@
+#/*************************************************************************
+#	> File Name: init_client.sh
+#	> Author: peranda
+#	> Mail:1365227825@qq.com  
+#	> Created Time: 2018年11月25日 星期日 20时14分33秒
+# ************************************************************************/
+#!/bin/bash
+for i in `seq 40 49`; do  
+    ssh peranda@192.168.1.${i} gcc -o ./client/1.test ./client/1.test.cpp -lpthread
+done 
