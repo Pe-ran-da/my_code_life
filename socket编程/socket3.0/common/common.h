6b6946d56b65281484869f971b039d8f3939e77f
peranda <1365227825@qq,com>
2018-12-24 16:06:22 +0800

    common

---
+++ socket编程/socket3.0/common/common.h
@@ -0,0 +1,32 @@
+/*************************************************************************
+	> File Name: common.h
+	> Author: peranda
+	> Mail:1365227825@qq.com  
+	> Created Time: 2018年11月18日 星期日 11时28分55秒
+ ************************************************************************/
+
+#ifndef _COMMON_H
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/file.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define _COMMON_H
+#endif
